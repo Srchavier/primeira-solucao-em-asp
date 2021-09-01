778dac76bb4bf7595440e4aacb2ba58bd3ccb018
Eduardo Fernandes <f577348@caixa.gov.com.br>
2021-09-01 00:03:47 -0300

    Adicionar arquivos de projeto.

---
+++ primeira solucao em asp/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="primeira_solucao_em_asp._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+
+    <div class="row">
+        <br />
+
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Inserir" OnClick="Button1_Click" />
+        <asp:Button ID="Button2" runat="server" Text="Listar" />
+        <asp:Button ID="Button3" runat="server" Text="Adicionar" />
+        <asp:Button ID="Button4" runat="server" Text="Deletar" />
+
+        <br />
+
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    </div>
+
+</asp:Content>
---
+++ primeira solucao em asp/Default.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace primeira_solucao_em_asp
+{
+    public partial class _Default : Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+
+            con.Open();
+            disp_data();
+        }
+
+   
+        public void disp_data()
+        {
+            try
+            {
+
+                {
+                    using (SqlCommand cCommand = new SqlCommand())
+                    {
+                        cCommand.Connection = con;
+                        cCommand.CommandType = CommandType.Text;
+
+                        cCommand.CommandText = "select * from table1";
+
+                        DataTable dtRetorno = new DataTable();
+
+                        using (SqlDataAdapter sqlAdapter = new SqlDataAdapter(cCommand))
+                        {
+                            sqlAdapter.Fill(dtRetorno);
+                            GridView1.DataSource = dtRetorno;
+                            GridView1.DataBind();
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into table1 values ('teste', 'teste' )";
+            cmd.ExecuteReader();
+            con.Close();
+
+            disp_data();
+        }
+    }
+}
